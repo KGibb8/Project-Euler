15f6043e8fde811672296b32a6d36d4316625a10
Kieran <kieran.a.gibb@gmail.com>
2016-05-27 10:34:09 +0100

    Sum primes made efficient, pythagorean triple solved

--- sum-primes.rb
+++ sum-primes.rb
@@ -3,17 +3,23 @@
 #Find the sum of all the primes below two million.
 
 class Primes
-  def sum_to(limit)
-    sum = 0
+  def self.sum_to(limit)
+    sum = 2
     n = 1
     while n < limit
         n += 1
         is_prime = true
         if n == 1
             is_prime = false
+            next
         end
 
-        for i in 2..n-1
+        if n.even?
+          is_prime = false
+          next
+        end
+
+        for i in 2..Math.sqrt(n)
             if n % i == 0
                 is_prime = false
             end
@@ -23,6 +29,7 @@ class Primes
             sum += n
         end
     end
+
     return sum
   end
 end
