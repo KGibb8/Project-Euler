32499ec563b0b921d5125d32b2603009860f7b37
Kieran <kieran.a.gibb@gmail.com>
2016-06-14 11:23:28 +0100

    number-letter-counts up to 1,000,000 | adapted nth_prime

--- nth-prime.rb
+++ nth-prime.rb
@@ -31,11 +31,51 @@ class Primes
       end
 
       if is_prime == true
+        # break if count == input
         primes.push(n)
       end
+      # count += 1
     end
     finish = Time.now
-    "Calculation took #{finish - start} seconds"
-    puts primes[input-1]
+    puts "Calculation took #{finish - start} seconds"
+    return primes[input-1]
   end
+
+
+  #############################################################
+  ###################### ANOTHER METHOD #######################
+  #############################################################
+
+
+    def nth_primeO(input)
+      start = Time.now
+      n = 0
+      count = 0
+      loop do
+        n += 1
+        next if n == 1
+
+        if n == 2
+          count += 1
+          break if count == input
+          next
+        end
+
+        next if n.even?
+
+        for i in 2..Math.sqrt(n)
+          if n % i == 0
+            is_prime = false
+          end
+        end
+
+        next if is_prime == false
+
+        count += 1
+        break if count == input
+      end
+      finish = Time.now
+      puts "Calculation took #{finish - start} seconds"
+      return n
+    end
 end
